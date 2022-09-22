6ed16b51a11d8c025407cc420c6495c980f56ffa
nurilloh2004 <nurillohpython@gmail.com>
2022-09-22 17:03:53 +0500

    ********

---
+++ keyboards/default/start.py
@@ -0,0 +1,12 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+
+menuMEn = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='Contact', request_contact=True),
+            KeyboardButton(text='Location', request_location=True),
+        ],
+    ],
+    resize_keyboard=True
+)
